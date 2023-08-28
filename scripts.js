891d2ef37d5ec990b4a6d134cdc803eea3e8da5e
AlphaCharlieFoxtrot <albertini.fanomezantsoa@gmail.com>
2023-08-28 23:56:17 +0200

    comments about adding some useful fonctiunality

---
+++ scripts.js
@@ -0,0 +1,5 @@
+/**un systême de réservation à créer donc un compte admin pour les gèrer 
++ système de comptes d'abonné pour pouvoir faire des réservation et payé en ligne si on veut
+(+rajouté un système de payement ou un lien vers paypal notamment...)*/
+
+//un slider a ajouter après la section intro notamment
