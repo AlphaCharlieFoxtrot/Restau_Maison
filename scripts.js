80292317f29a72d90574f68b1cf569d90beef269
AlphaCharlieFoxtrot <albertini.fanomezantsoa@gmail.com>
2023-08-28 23:59:30 +0200

    same as the last comment

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 /**un systême de réservation à créer donc un compte admin pour les gèrer 
 + système de comptes d'abonné pour pouvoir faire des réservation et payé en ligne si on veut
-(+rajouté un système de payement ou un lien vers paypal notamment...)*/
+(+rajouté un système de payement ou un lien vers paypal notamment...la sécurité informatique c'est pas ma spé)*/
 
 //un slider a ajouter après la section intro notamment
