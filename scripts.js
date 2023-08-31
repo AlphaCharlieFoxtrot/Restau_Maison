b02164387b585bae5c15125e3cedb9f19b71d3fc
AlphaCharlieFoxtrot <albertini.fanomezantsoa@gmail.com>
2023-08-31 23:16:42 +0200

    navigateur Edge + Modzilla ok

--- scripts.js
+++ scripts.js
@@ -3,3 +3,28 @@
 (+rajouté un système de payement ou un lien vers paypal notamment...la sécurité informatique c'est pas ma spé)*/
 
 //un slider a ajouter après la section intro notamment
+
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false, prevPageX, perScrollLeft;
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX
+    prevScrollLeft = carousel.scrollLeft
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false
+}
+
+carousel.addEventListener("mousedown", dragStart)
+carousel.addEventListener("mousemove", dragging)
+carousel.addEventListener("mouseup", dragStop)
